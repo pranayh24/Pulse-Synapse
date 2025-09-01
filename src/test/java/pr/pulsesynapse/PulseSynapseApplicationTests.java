b47bf19128d71e0ef243bb92d457afaa8507c637
pranayh24 <pranayheda24@gmail.com>
2025-09-01 16:03:33 +0530

    First Commit

---
+++ src/test/java/pr/pulsesynapse/PulseSynapseApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.pulsesynapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseSynapseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
