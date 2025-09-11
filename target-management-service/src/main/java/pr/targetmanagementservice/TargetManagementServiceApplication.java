f20a3320d3bf98e661c9246ea25778fb770f7fc8
pranayh24 <pranayheda24@gmail.com>
2025-09-12 02:09:56 +0530

    Add configuration for target management service and initial application setup

---
+++ target-management-service/src/main/java/pr/targetmanagementservice/TargetManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package pr.targetmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetManagementServiceApplication.class, args);
+    }
+
+}
